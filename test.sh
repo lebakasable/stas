b47a5af0d09d783d2b7b3bb41783520d7e7b51fe
LeBakaSable <hugogole.ito@gmail.com>
2024-11-10 19:54:10 +0100

    feat: enhance test.sh

--- test.sh
+++ test.sh
@@ -2,26 +2,40 @@
 
 ./build.sh
 
+function print_error {
+   echo
+   echo "Error!"
+   echo "Wanted: '$(echo "$2" | sed ':a;N;$!ba;s/\n/\\n/g')'"
+   echo "But got: '$(echo "$3" | sed ':a;N;$!ba;s/\n/\\n/g')'"
+   exit
+}
+
 function try {
-   if echo "$1" | ./stas | grep -q "^$2\$"
+   if [ "$(echo "$1" | ./stas)" = "$2" ]
    then
-      printf '.'
+      printf .
    else
-      echo
-      echo "Error!"
-      echo "Wanted:"
-      echo "-------------------------------------------"
-      echo "$1"
-      echo "$2"
-      echo "-------------------------------------------"
-      echo
-      echo "But got:"
-      echo "-------------------------------------------"
-      echo "$1"
-      echo "$1" | ./stas
-      echo "-------------------------------------------"
-      echo
-      exit
+      print_error "$1" "$2" "$(echo "$1" | ./stas)"
+   fi
+}
+
+function try_file {
+   actual=$(./stas < $1)
+   if [ "$actual" = "$2" ]
+   then
+      printf .
+   else
+      print_error "$(cat "$1")" "$2" "$actual"
+   fi
+}
+
+function try_exec {
+   actual=$($1)
+   if [ "$actual" = "$2" ]
+   then
+      printf .
+   else
+      print_error "$1" "$2" "$actual"
    fi
 }
 
@@ -48,7 +62,7 @@ try '5 dup print-stack'            '5 5 '
 try '5 9 swap print-stack'         '9 5 '
 try '5 9 over print-stack'         '5 9 5 '
 
-try '"Hello$\\\$\n" print'         'Hello$\\$'
+try '"Hello$\$\n" print'           'Hello$$'
 
 try '"Hello" print-line'           'Hello'
 
@@ -77,11 +91,22 @@ try ': t "true" print ;
 
 try ': hi "Hi!" print ;
      : maybe-hi if? hi ;
-     inspect hi
-     inspect maybe-hi
      0 maybe-hi
      1 maybe-hi
      "\n" print'                   'Hi!'
+
+try_file examples/hello.stas "Wrote to 'hello4'"
+try_exec ./hello4 "Hello, World!
+Hello, World!
+Hello, World!
+Hello, World!"
+rm hello4
+
+try_file examples/loop.stas "5 4 3 2 1 
+10 9 8 7 6 5 4 3 2 1 "
+
+try_file examples/euler/001.stas '233168'
+try_file examples/euler/002.stas '4613732'
 
 echo
 echo Passed!
